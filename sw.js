ffc60a13a910841c70c8798bda6ae301467bf40d
mahd349 <mahdimoslem349@gmail.com>
2026-09-11 14:02:47 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline support + stale-while-revalidate caching
 ================================================================ */
 
-const CACHE = "routine-v8";
+const CACHE = "routine-v9";
 
 const CORE = [
   "/",
