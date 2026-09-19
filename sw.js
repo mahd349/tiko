6f360819ae8ff5d9aedb20606212b8d545447fc8
mahd349 <mahdimoslem349@gmail.com>
2026-09-19 19:53:18 +0330

    Update service worker cache version to v21

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline support + stale-while-revalidate caching
 ================================================================ */
 
-const CACHE = "routine-v20";
+const CACHE = "routine-v21";
 
 const CORE = [
   "/",
