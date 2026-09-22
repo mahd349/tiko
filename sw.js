3f7853195843152f45a4f3c925912369adcad139
mahd349 <mahdimoslem349@gmail.com>
2026-09-22 10:33:50 +0330

    Update service worker cache version to v30

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline support + stale-while-revalidate caching
 ================================================================ */
 
-const CACHE = "routine-v29";
+const CACHE = "routine-v30";
 
 const CORE = [
   "/",
@@ -28,6 +28,7 @@ const CORE = [
   "/assets/js/features/auth.js",
    "/assets/js/features/reminder.js",
    "/assets/js/features/fx-fireball-bg.js",
+   "/assets/js/features/rewards.js",
   "/assets/js/ui/modal.js",
   "/assets/js/ui/toast.js",
   "/assets/js/app.js",
