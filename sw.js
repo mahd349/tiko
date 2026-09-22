4ce69fd8ca29e5d4ff94957f3e30ceb349d5065f
mahd349 <mahdimoslem349@gmail.com>
2026-09-22 13:13:31 +0330

    Update service worker cache version to v34

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline support + stale-while-revalidate caching
 ================================================================ */
 
-const CACHE = "routine-v33";
+const CACHE = "routine-v34";
 
 const CORE = [
   "/",
