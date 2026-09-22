a8861fb1d5498f0452ce8463e0c475121435bace
mahd349 <mahdimoslem349@gmail.com>
2026-09-22 09:59:13 +0330

    Update service worker cache version to v26

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline support + stale-while-revalidate caching
 ================================================================ */
 
-const CACHE = "routine-v25";
+const CACHE = "routine-v26";
 
 const CORE = [
   "/",
